817ed3e7edf269952e4ebdbcb7b85a522d1d0033
Charline Laporte <charlinelaporte@MacBook-Pro-de-Charline.local>
2016-03-18 19:43:17 +0100

    hello

---
+++ script.js
@@ -0,0 +1,11 @@
+// LINKE
+
+
+var appMap = {
+  init: function () {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 45.7546102, lng: 4.8573142},
+      zoom: 13
+    });
+  }
+}
